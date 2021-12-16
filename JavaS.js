07a148b51fc0089c96371890c4f850bf1827ea75
julia g tolentino <93351992+juliag33@users.noreply.github.com>
2021-12-16 19:49:06 -0300

    Add files via upload

---
+++ JavaS.js
@@ -0,0 +1,6 @@
+let nome = prompt("Digite seu nome aqui");
+let texto = `Seja bem-vindo(a) ${nome}`
+alert(`Olá ${nome}, seja bem-vindo (a)`)
+if (nome ==0){
+    alert(`Olá pessoa, seja bem-vinda`)
+}
